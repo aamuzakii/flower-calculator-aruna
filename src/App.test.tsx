38ebca98e3e5af4247568290794c711e73cad00c
zaki <abdullah.muzaki@alodokter.com>
2022-07-17 19:53:43 +0700

    Test sum when all checked

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
-test('Parent testing', () => {
+test('Test sum when all checked', () => {
   const app = render(<App />);
+  // user input 3 fields
+  userEvent.type(screen.getAllByRole('number-input')[0], '1');
+  userEvent.type(screen.getAllByRole('number-input')[1], '2');
+  userEvent.type(screen.getAllByRole('number-input')[2], '3');
+
+  // user checkbox
+  userEvent.click(screen.getAllByRole('checkbox')[0]);
+  userEvent.click(screen.getAllByRole('checkbox')[1]);
+  userEvent.click(screen.getAllByRole('checkbox')[2]);
+  // click on operation
+  userEvent.click(screen.getByText('+'));
+
+  // get the result
+  expect(screen.getByTestId('result-fragment')).toHaveTextContent('Hasil: 6');
 
 });
