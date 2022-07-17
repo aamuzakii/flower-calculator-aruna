3240b754365214ee3991e937b656e1aaeb932fa3
zaki <abdullah.muzaki@alodokter.com>
2022-07-17 20:35:35 +0700

    Reach max coverage limit in App.test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -62,3 +62,31 @@ test('Test divide when all checked', () => {
   expect(screen.getByTestId('result-fragment')).toHaveTextContent('Hasil: 10');
 
 });
+
+test('Test sum when all unchecked', () => {
+  render(<App />);
+  const jsdomAlert = window.alert;  // remember the jsdom alert
+  window.alert = () => {};  // provide an empty implementation for window.alert
+  userEvent.type(screen.getAllByRole('number-input')[0], '1');
+  userEvent.type(screen.getAllByRole('number-input')[1], '2');
+  userEvent.type(screen.getAllByRole('number-input')[2], '3');
+
+  userEvent.click(screen.getByText('+'));
+
+  expect(screen.getByTestId('result-fragment')).toHaveTextContent('Hasil: 0');
+  window.alert = jsdomAlert;  // restore the jsdom alert
+})
+
+test('Test when there is a NaN operation case', () => {
+  render(<App />);
+  const jsdomAlert = window.alert;  
+  window.alert = () => {};  
+  userEvent.type(screen.getAllByRole('number-input')[0], '1');
+
+  userEvent.click(screen.getAllByRole('checkbox')[0]);
+  userEvent.click(screen.getAllByRole('checkbox')[1]); // checked but no input so will cause NaN
+  userEvent.click(screen.getByText('+'));
+
+  expect(screen.getByTestId('result-fragment')).toHaveTextContent('Hasil: 0');
+  window.alert = jsdomAlert;  
+})
